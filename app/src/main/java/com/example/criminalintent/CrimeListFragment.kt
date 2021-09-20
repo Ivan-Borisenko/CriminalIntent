b52a1a380e26787410910775439211e17354d841
Ivan-Borisenko <ivan.v.borisenko@gmail.com>
2021-09-20 17:18:23 +0300

    Toolbar with custom addButton added

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -3,9 +3,7 @@ package com.example.criminalintent
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -37,10 +35,28 @@ class CrimeListFragment : Fragment() {
         callbacks = context as? Callbacks
     }
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_crime_list, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            } else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }*/   //ссылался на CrimeListViewModel.crimes, которого больше не существует
+        setHasOptionsMenu(true)
+        //Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+        //ссылался на CrimeListViewModel.crimes, которого больше не существует
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
